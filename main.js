a667088ddf0f837dca3e8dff4dd13087759e93a8
laibay <150113457+laibay19@users.noreply.github.com>
2023-11-23 11:58:54 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-function table(){
-    let num = document.getElementById("input").value
-     
-    for (let i=1 ; i<=10 ; num++){
-        let table  = num + "x" + i + "=" + num*i
+function table() {
 
+    document.getElementById("result").innerHTML = ""
+
+    let num = +document.getElementById("input").value
+    let from = +document.getElementById("from").value
+    let to = +document.getElementById("to").value
+
+    if (from >= to) {
+        document.getElementById("result").innerHTML = "Number is greater"
+        return;
+    }
+
+    for (let i = from; i <= to; i++) {
+
+        let table = `${num} x ${i} = ${num * i}` 
 
         let output = document.getElementById("result")
-        // output.innerHTML += ("table")
-        console.log(table)
+
+        output.innerHTML += `${table} <br/>`
+
     }
+
 }
